144af1b52c0e1b640e6ef8a96f5fde99c7efaeb7
Ratana10 <sanratana18@gmail.com>
2024-05-30 17:13:25 +0700

    add InmemoryUserDetail

--- src/main/java/com/piseth/schoolapi/roles/RoleEnum.java
+++ src/main/java/com/piseth/schoolapi/roles/RoleEnum.java
@@ -1,6 +1,31 @@
 package com.piseth.schoolapi.roles;
 
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum RoleEnum {
-    STUDENT,
-    ADMIN
+    ADMIN(Set.of(
+            PermissionEnum.CATEGORY_READ,
+            PermissionEnum.CATEGORY_WRITE,
+            PermissionEnum.COURSE_WRITE,
+            PermissionEnum.COURSE_READ
+    )),
+    USER(Set.of(
+            PermissionEnum.COURSE_READ
+    ));
+    private Set<PermissionEnum> permissions;
+    public Set<SimpleGrantedAuthority> getAuthorities(){
+        Set<SimpleGrantedAuthority> grantedAuthorities = permissions.stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getDescription()))
+                .collect(Collectors.toSet());
+
+        SimpleGrantedAuthority role = new SimpleGrantedAuthority("ROLE_"+this.name());
+        grantedAuthorities.add(role);
+        return grantedAuthorities;
+    }
 }
